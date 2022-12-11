2bba7a6e29171cb0ca6c2eb8e53900aa3e243ace
Brook Nega <brooknega21@gmail.com>
2022-12-10 22:41:15 -0500

    first fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import HomePage from './HomePage';
 import NavBar from "./NavBar";
 import { Routes, Route, BrowserRouter } from "react-router-dom"
@@ -7,8 +7,30 @@ import Account from './Account';
 import Notifications from './Notifications';
 import 'semantic-ui-css/semantic.min.css'
 
+const loginUrl = "http://localhost:3000/login"
 
 function App() {
+
+  useEffect(()=>{
+    if (localStorage.uid)
+    console.log("User found:", localStorage.uid)
+    else
+    console.log("No user found")
+  }, [])
+
+  fetch( loginUrl, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify({
+      email: 'brook@gmail.com',
+      password: '12345_Bb'
+    })
+  })
+  .then( r => r.json())
+  .then( user => localStorage.uid = user.uid)
   return (
       <div>
             <NavBar/>
